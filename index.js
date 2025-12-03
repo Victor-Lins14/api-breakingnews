b8cb9c1b0f32fa5404f8a1d0a866d1a23e598f41
Victor Lins <vlinsgurgel@gmail.com>
2025-12-03 08:59:40 -0300

    Commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 const port = 3000;
 
+app.use(express.json())
 app.use('/user', userRoute)
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,22 @@
 const create = (req, res) => {
-    const user = req.body;
+    const {name, username, email, password, avatar, background} = req.body;
 
-    console.log(user);
+    if (!name || !username || !email || !password, !avatar || !background) {
+        res.status(400).send({message: "Submit all fields for registration"})
+    }
+
+    
+
+    res.status(201).send({
+        message: "User created successfully",
+        user: {
+            name,
+            username,
+            email,
+            avatar,
+            background
+        }
+    })
 }
 
 module.exports = { create };
